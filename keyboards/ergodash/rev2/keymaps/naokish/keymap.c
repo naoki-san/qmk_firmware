aa9e0d41421fec9cbc9cf186d14edbcb4b284f93
naoki-san <dev@blue-works.net>
2019-02-10 17:03:11 +0900

    [Keymap] Fixed keymap.

--- keyboards/ergodash/rev2/keymaps/naokish/keymap.c
+++ keyboards/ergodash/rev2/keymaps/naokish/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                        KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,  \
     KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_BSLS,                        KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT , \
-    KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    EISU,                         KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    EISU,                           KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
     LOWER,   KC_LALT,KC_LGUI, KC_LSFT,          KC_RCTL, LOWER,  KC_MINS,    KC_BSPC,  RAISE , CODING,            KC_RSFT, KC_RGUI, KC_RALT, LOWER    \
   ),
 
@@ -105,8 +105,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_CODING] = LAYOUT(
     KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_UNDS,                        KC_EQL,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,  \
-    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_EXLM, KC_EQL,  KC_LT,                          KC_GT,   KC_LPRN, KC_RPRN, KC_ASTR, KC_UNDS, KC_F12,  KC_BSPC, \
-    KC_LCTL, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_MINS, KC_QUOT,                        KC_DQT,  KC_LCBR, KC_RCBR, KC_K,    KC_COLN, KC_SCLN, KC_ENT , \
+    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_EXLM, KC_EQL,  KC_LT,                          KC_GT,   KC_LPRN, KC_RPRN, KC_COMM, KC_UNDS, KC_F12,  KC_BSPC, \
+    KC_LCTL, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_MINS, KC_QUOT,                        KC_DQT,  KC_LCBR, KC_RCBR, KC_DOT,  KC_COLN, KC_SCLN, KC_ENT , \
     KC_LSFT, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_SLSH, KC_ESC,                         KC_DEL , KC_LBRC, KC_RBRC, KC_AMPR, KC_PIPE, KC_QUES, KC_RSFT, \
     LOWER,   KC_LALT, KC_0, KC_LSFT,             KC_RCTL, LOWER,  KC_MINS,      KC_BSPC,  RAISE,   CODING,           KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
   ),
